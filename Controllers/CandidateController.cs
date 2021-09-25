87b749d4fa007d509785b6a54c4ca0a55d1a4fe9
kathiresangit <90887754+kathiresangit@users.noreply.github.com>
2021-09-25 19:25:11 +0530

    Default1

--- Controllers/CandidateController.cs
+++ Controllers/CandidateController.cs
@@ -16,7 +16,7 @@ namespace DemoProject.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Candidate1", "Candidate2","Candidate3" };
+            return new string[] { "Candidate1", "Candidate2","Candidate3","Candidate4" };
         }
 
         // GET api/<CandidateController>/5
