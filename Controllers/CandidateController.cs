385cf8ad04071f916db587b2b834a3851633297a
kathiresangit <90887754+kathiresangit@users.noreply.github.com>
2021-09-25 19:20:23 +0530

    commit check1

--- Controllers/CandidateController.cs
+++ Controllers/CandidateController.cs
@@ -16,7 +16,7 @@ namespace DemoProject.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Candidate1", "Candidate2" };
+            return new string[] { "Candidate1", "CandidateBB" };
         }
 
         // GET api/<CandidateController>/5
